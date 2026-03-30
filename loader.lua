045ee639137d614a05265bdab29feb2c6164dc9f
Bananashifty <owen.tran.2010@gmail.com>
2026-03-30 20:40:33 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "🍌 BANANA HUB | KEY SYSTEM",
-   LoadingTitle = "Banana Hub V72",
+   LoadingTitle = "Loading Banana V70...",
    LoadingSubtitle = "by Bananashifty",
    ConfigurationSaving = { Enabled = false }
 })
@@ -14,16 +14,12 @@ Tab:CreateInput({
    PlaceholderText = "Paste key here...",
    Callback = function(Text)
        if Text == "BANANA_V70_SECRET" then
-           Rayfield:Notify({Title = "Access Granted!", Content = "FORCING V70 LOAD...", Duration = 5})
-           
-           -- ON FERME RAYFIELD POUR LAISSER PLACE AU MENU NOIR
-           Window:Destroy() 
-           
-           -- CE LIEN EST LE SEUL QUI COMPTE :
-           local latest_code = "https://raw.githubusercontent.com/Bananashifty/v70_source.lua/refs/heads/main/v70_source.lua?t=" .. os.time()
-           loadstring(game:HttpGet(latest_code))()
+           Rayfield:Notify({Title = "Success", Content = "Loading your V70 Code...", Duration = 5})
+           Window:Destroy()
+           -- CECI CHARGE TON CODE SUR GITHUB :
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/Bananashifty/v70_source.lua/refs/heads/main/v70_source.lua?t=" .. os.time()))()
        else
-           Rayfield:Notify({Title = "Invalid Key", Content = "Check your key.", Duration = 5})
+           Rayfield:Notify({Title = "Error", Content = "Wrong Key!", Duration = 5})
        end
    end,
 })
