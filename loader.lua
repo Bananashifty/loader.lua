9d25e34f626b18964497a48de30379ec12e363f9
Bananashifty <owen.tran.2010@gmail.com>
2026-03-30 20:34:19 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,32 +7,23 @@ local Window = Rayfield:CreateWindow({
    ConfigurationSaving = { Enabled = false }
 })
 
-local KeySettings = {
-   Title = "Banana Hub | Access Key",
-   Subtitle = "Get your key from LootLabs",
-   Note = "Link: https://loot-link.com/s?kc5LObQK",
-   SaveKey = true,
-   Key = {"BANANA_V70_SECRET"}
-}
-
 local Tab = Window:CreateTab("Key Entry", 4483362458)
 
 Tab:CreateInput({
    Name = "Enter Key",
    PlaceholderText = "Paste key here...",
    Callback = function(Text)
-       if Text == KeySettings.Key[1] then
-           Rayfield:Notify({Title = "Access Granted!", Content = "Loading V72...", Duration = 5})
-           task.wait(1)
-           -- LE LIEN AVEC FORCE UPDATE (os.time)
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/Bananashifty/v70_source.lua/refs/heads/main/v70_source.lua?t=" .. os.time()))()
+       if Text == "BANANA_V70_SECRET" then
+           Rayfield:Notify({Title = "Access Granted!", Content = "FORCING V70 LOAD...", Duration = 5})
+           
+           -- ON FERME RAYFIELD POUR LAISSER PLACE AU MENU NOIR
+           Window:Destroy() 
+           
+           -- CE LIEN EST LE SEUL QUI COMPTE :
+           local latest_code = "https://raw.githubusercontent.com/Bananashifty/v70_source.lua/refs/heads/main/v70_source.lua?t=" .. os.time()
+           loadstring(game:HttpGet(latest_code))()
        else
            Rayfield:Notify({Title = "Invalid Key", Content = "Check your key.", Duration = 5})
        end
    end,
-})
-
-Tab:CreateButton({
-   Name = "Get Key (Copy Link)",
-   Callback = function() setclipboard("https://loot-link.com/s?kc5LObQK") end,
 })
